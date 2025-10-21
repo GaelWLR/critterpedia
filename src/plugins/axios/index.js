eedb2dc5ccc766f64a28fe68ffea7cc559203f5b
Gaël <weiler.gael@gmail.com>
2025-10-21 03:04:02 +0200

    chore(deps): add yarn configuration and package lock files
    Configure Yarn with node-modules linker and add package-lock.json for dependency management

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
-const acnhApi = axios.create({ baseURL: 'https://acnhapi.com/v1a/' })
+const acnhApi = axios.create({
+  baseURL: '/data/'
+})
 
-const bugs = () => acnhApi.get('bugs')
-const fishes = () => acnhApi.get('fish')
-const fossils = () => acnhApi.get('fossils')
+const bugs = () => acnhApi.get('bugs.json')
+const fishes = () => acnhApi.get('fish.json')
+const fossils = () => acnhApi.get('fossils.json')
 
 export { bugs, fishes, fossils }
